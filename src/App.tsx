cdbd22696255be67e248a608bea803d880a79097
J <josephmonge00@gmail.com>
2024-05-20 16:13:57 -0700

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import "./assets/App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-xl">init</h1>
+    </>
+  );
+}
+
+export default App;
